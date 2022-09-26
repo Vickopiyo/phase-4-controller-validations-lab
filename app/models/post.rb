0f31e3f81da1f03d91db2e59ca385698fa532481
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-26 15:00:08 +0300

    DONE

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title,  presence: true
+    validates :category, inclusion: { in: ["Fiction","Non-Fiction"] } 
+    validates :content, presence: true, length: { minimum:100 }
+    
 end
