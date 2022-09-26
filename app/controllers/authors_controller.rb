212ffa68d40b3abb273d5035ad7339dff3e1d89c
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-26 14:59:54 +0300

    commit tree 9aa164d8d4c91e77315a0288f0ff5390e6474f9c

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,8 @@
 class AuthorsController < ApplicationController
+
+# rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
   def show
     author = Author.find(params[:id])
@@ -7,12 +11,24 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+
+     rescue ActiveRecord::RecordInvalid => invalid
+
+      render json: { errors: invalid.record.errors   },status: :unprocessable_entity
   end
 
   private
+  # def render_not_found_response
+  #   render json: { error: "Auhtor not found" }, status: :unprocessable_entity
+  # end
+
+
+  # def invalid_record(invalid) 
+
+  #   render json: { errors: invalid.record.errors.full_messages  },status: :unprocessable_entity
+  # end
   
   def author_params
     params.permit(:email, :name)
